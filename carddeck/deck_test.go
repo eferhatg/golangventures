5ab33c02ab3bbaeda7a4d568c8a4f82789f3badc
eferhatg <eferhatg@gmail.com>
2018-10-16 02:00:53 +0300

    ssh test

--- carddeck/deck_test.go
+++ carddeck/deck_test.go
@@ -8,10 +8,12 @@ import (
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
+
 		t.Errorf("Expected 52 but got %v", len(d))
 	}
 
 	if d[0] != "Ace of Spades" {
+
 		t.Errorf("Expected Ace of Spades but got %s", d[0])
 	}
 
