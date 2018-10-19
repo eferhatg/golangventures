af12ec65339ef8b3dece2752c20322503c19ef0e
eferhatg <eferhatg@gmail.com>
2018-10-19 22:40:44 +0300

    interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hi There!"
+}
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
